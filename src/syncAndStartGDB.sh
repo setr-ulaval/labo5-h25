4031c671665b1dcd24c9bb76102b42e4f22aa22c
Marc-Andre Gardner <marc.andre.gardner@gmail.com>
2023-03-31 03:18:17 -0400

    Correction du probleme de lancement de GDB

--- src/syncAndStartGDB.sh
+++ src/syncAndStartGDB.sh
@@ -7,6 +7,6 @@ rpiaddr="ADRESSE DE VOTRE RASPBERRY PI ICI"
 
 rsync -az $1/build/emulateurClavier $1/createurRequetes/createurRequetes "pi@$rpiaddr:/home/pi/projects/laboratoire5/"
 # Execute GDB
-ssh "pi@$rpiaddr" "nohup /home/pi/projects/laboratoire5/createurRequetes fichierComm 60 120 10 40 1 0 & nohup sudo gdbserver :$3 /home/pi/projects/laboratoire5/emulateurClavier fichierComm 10000 > /home/pi/capture-stdout-$2 2> /home/pi/capture-stderr-$2 < /dev/null &"
+ssh "pi@$rpiaddr" "nohup /home/pi/projects/laboratoire5/createurRequetes fichierComm 60 120 10 40 1 0 </dev/null >/home/pi/capture-createurRequetes 2>&1 & nohup sudo gdbserver :$3 /home/pi/projects/laboratoire5/emulateurClavier fichierComm 10000 > /home/pi/capture-stdout-$2 2> /home/pi/capture-stderr-$2 < /dev/null &"
 sleep 1
 
